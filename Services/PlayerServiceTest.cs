31c8bf00e1fc27792add6e784807992428619982
VictorHugoNeivaDosSantos <victorhugoneiva2017@gmail.com>
2021-10-12 10:57:47 -0300

    Implementaçã de novos testes

--- Services/PlayerServiceTest.cs
+++ Services/PlayerServiceTest.cs
@@ -26,13 +26,48 @@ namespace JokenpoTest.Services
             _servicePlayer = new PlayerService(_repositoryPlayer.Object, mapConfig.CreateMapper());
         }
 
+        [Test]
         public void AddPlayerTest()
         {
-            var player = new PlayerDto
+            var playerDto = new PlayerDto
             {
                 Name = "Test",
+                Email = "teste"
             };
-           
+
+            Assert.DoesNotThrow(() => _servicePlayer.AddPlayer(playerDto));
         }
+
+        [Test]
+        public void AddPlayerTest_NotEmail()
+        {
+            var playerDto = new PlayerDto
+            {
+                Name = "Test",
+ 
+            };
+          
+
+            Assert.Throws<Exception>(() => _servicePlayer.AddPlayer(playerDto));
+        }
+
+        [Test]
+        public void GetPlayerByIdTest()
+        {
+            var player = new Player
+            {
+                Id = Guid.Parse("50d72022-4ae8-4ee0-96ff-7470fc4719bf"),
+                Email = "teste",
+                Name = "teste",
+                Status = Jokenpo.Enuns.StatusPlayer.Ativo
+            };
+
+            var list = new List<Player>();
+            _repositoryPlayer.Setup(s => s.GetPlayerById(player.Id)).Returns(player);
+           
+            Assert.AreEqual("teste", _servicePlayer.GetPlayerById(player.Id).Name);
+        }
+
+
     }
 }
